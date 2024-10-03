45011e9a5aadc086617536ae463cc0f76c539132
Jules Herrmann <jherrmann@stp-2005-50.etu.sg.lan>
2024-10-03 11:45:20 +0200

    fix tp2

--- TP2_exo2.py
+++ TP2_exo2.py
@@ -1,9 +1,11 @@
 import numpy as np
 
 class Task():
-    def __init__(self,debut,fin):
+    def __init__(self,debut,fin,debut_inf = False, fin_inf = False):
         self.debut = debut
+        self.debut_inf = debut_inf
         self.fin = fin
+        self.fin_inf = fin_inf
 
     def __repr__(self):
         return f"Task(d={self.debut},f={self.fin})"
@@ -14,7 +16,7 @@ def creer_liste_activite(n,tmax,dmax):
     return [Task(t,t+d) for (t,d) in zip(tlist,dlist)]
 
 def choix_activite_rec(A,i,j):
-    Aij = list(filter(lambda k : i.fin <= k.debut and k.fin <= j.debut,A))
+    Aij = list(filter(lambda k : (i.fin <= k.debut or i.fin_inf) and (k.fin <= j.debut or j.debut_inf), A))
     if len(Aij) == 0:
         return (0,Aij)
     best_size = 0
@@ -40,6 +42,23 @@ def choix_activite_glouton(A):
 
     return C
 
+def perf_test(maxl,nbl,algo,log_scale=True):
+    # breakpoint()
+    if log_scale :
+        lengths = np.geomspace(1,maxl,nbl).astype(int)
+    else :
+        lengths = np.linspace(1,maxl,nbl).astype(int)
+    perf_time = np.zeros(nbl)
+    for i,l in enumerate(lengths):
+        tasks = creer_liste_activite(l,l,int(l/3)+2)
+
+        t1 = time.perf_counter()
+        algo(tasks)
+        perf_time[i] = time.perf_counter() - t1
+
+    plt.plot(lengths,perf_time)
+    plt.show()
+
 if __name__=="__main__":
     tasks = creer_liste_activite(5,5,4)
     print(tasks)
@@ -50,3 +69,5 @@ if __name__=="__main__":
 
     best_set2 = choix_activite_glouton(tasks)
     print(best_set2)
+
+    perf_test(1000,20,choix_activite_glouton)
